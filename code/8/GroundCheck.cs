2cce76696b7ff4a030012560cde9122dd8b665be
김태현 <45249430+MozziDog@users.noreply.github.com>
2020-04-01 22:23:01 +0900

    added source file for chapter 8

---
+++ code/8/GroundCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public Player player;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player.leftJump = 2;
+        player.isGrounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        player.isGrounded = false;
+    }
+
+}
