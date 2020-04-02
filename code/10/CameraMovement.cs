e8bdc5aeded4a724291c90a943d6c9fce827c59a
김태현 <45249430+MozziDog@users.noreply.github.com>
2020-04-03 01:38:54 +0900

    added source file for chapter 10

---
+++ code/10/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float smoothTime;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float position_x, position_y;
+        float tmp_x=0, tmp_y = 0;
+        position_x = Mathf.SmoothDamp(gameObject.transform.localPosition.x,
+                                    player.transform.localPosition.x, ref tmp_x, smoothTime, 10);
+        position_y = Mathf.SmoothDamp(gameObject.transform.localPosition.y,
+                                    player.transform.localPosition.y, ref tmp_y, smoothTime, 10);
+        position_x = Mathf.Clamp(position_x, minPosition.x, maxPosition.x);
+        position_y = Mathf.Clamp(position_y, minPosition.y, maxPosition.y);
+        gameObject.transform.localPosition = new Vector3(position_x, position_y, gameObject.transform.localPosition.z);
+    }
+}
