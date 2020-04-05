c765bd8a417df5b9221bfb9e91b7718e5711d13b
김태현 <45249430+MozziDog@users.noreply.github.com>
2020-04-05 14:28:40 +0900

    added source file for chapter 11,12,13

---
+++ code/12/Coin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip sound;
+    public GameManager gameManager;
+    public GameObject effect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            // 스코어 100 증가
+            gameManager.score += 100;
+            // 사운드 출력
+            audioSource.PlayOneShot(sound);
+            // 이펙트 오브젝트 생성
+            Instantiate(effect, gameObject.transform.localPosition, Quaternion.Euler(0,0,0));
+            // 현재 게임오브젝트 삭제
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ code/13/Coin.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip sound;
+    public GameManager gameManager;
+    public GameObject effect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 태그로 게임오브젝트 찾기
+        GameObject gameManagerObject = GameObject.FindGameObjectWithTag("GameManager");
+        // GetComponent로 게임오브젝트에 부착된 컴포넌트 찾기 1
+        gameManager = gameManagerObject.GetComponent<GameManager>();
+        // GetComponent로 게임오브젝트에 부착된 컴포넌트 찾기 2
+        audioSource = (AudioSource)(gameManagerObject.GetComponent(typeof (AudioSource)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            // 스코어 100 증가
+            gameManager.score += 100;
+            // 사운드 출력
+            audioSource.PlayOneShot(sound);
+            // 이펙트 오브젝트 생성
+            GameObject effectObject = Instantiate(effect, gameObject.transform.localPosition, Quaternion.Euler(0,0,0));
+            // 이펙트 오브젝트 2초 후 삭제
+            Destroy(effectObject, 2f);
+            // 현재 게임오브젝트 삭제
+            Destroy(gameObject);
+        }
+    }
+}
